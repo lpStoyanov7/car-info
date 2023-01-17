fb85ed13342ad8ea3bab26f3eb93f89ef1873780
stoystbg <stoyan.stoyanov@bbraun.com>
2023-01-17 17:11:13 +0200

    fixing db scripts

--- sql/V1_1__initial_schema.sql
+++ sql/V1_1__initial_schema.sql
@@ -1,19 +1,19 @@
 create table tbl_manufacturer
 (
     id               int           not null PRIMARY KEY,
-    name             nvarchar(255) not null,
-    country          nvarchar(255),
+    name             varchar(255) not null,
+    country          varchar(255),
     year_of_founding datetime2
 );
-go
+
 create table tbl_engine
 (
     id       int not null PRIMARY KEY,
     power    int,
     capacity int,
-    name     nvarchar(255)
+    name     varchar(255)
 );
-go
+
 create table tbl_car
 (
     id              int not null PRIMARY KEY,
