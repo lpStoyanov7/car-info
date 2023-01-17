0489eddc956d5ab6a066615ef71ab8f032c4a00a
stoystbg <stoyan.stoyanov@bbraun.com>
2023-01-17 17:33:45 +0200

    adding wrong script on purpose

--- sql/V1_1__initial_schema.sql
+++ sql/V1_1__initial_schema.sql
@@ -19,11 +19,11 @@ create table tbl_car
     id              int not null PRIMARY KEY,
     engine_id       int not null,
     manufacturer_id int not null,
-    year            DATE,
+    year            DATE3,
     constraint fk_car_engine
         foreign key (engine_id)
             REFERENCES tbl_engine (id),
-    constraint fk_car_manufacturer4
+    constraint fk_car_manufacturer
         foreign key (manufacturer_id)
             REFERENCES tbl_manufacturer (id)
 );
